d1f99f914b45fa8438cc49c16a799f9b80f9bfec
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-11-16 11:44:46 +0200

    Add files via upload

---
+++ Polymorphism and Abstraction/Exercise/05. Animals/tomcat.py
@@ -0,0 +1,16 @@
+from project.cat import Cat
+
+class Tomcat(Cat):
+
+
+  def __init__(self, name, age):
+    super().__init__(name, age, "Male")
+
+
+
+  def __repr__(self):
+    return f"This is {self.name}. {self.name} is a {self.age} year old Male {__class__.__name__}"
+
+
+  def make_sound(self):
+    return "Hiss"
