852f79b93cc20f853465ecdff4d12cb6078d7854
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-11-17 12:36:20 +0200

    Create 02. Reverse Iter.py

---
+++ Iterators and Generators/Lab/02. Reverse Iter.py
@@ -0,0 +1,22 @@
+class reverse_iter:
+
+    def __init__(self, iterable):
+        self.iterable = iterable
+        self.i = len(self.iterable) - 1
+
+
+    def __iter__(self):
+        return self
+    
+
+
+    def __next__(self):
+
+        if self.i < 0:
+           raise StopIteration
+        
+        
+        toprint = self.i
+        self.i -= 1
+
+        return self.iterable[toprint]
